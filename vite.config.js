2c1830d1c2a3fbeb428335f8dd17ff943150601b
imhvit <yosifblancotafur2006@gmail.com>
2026-04-10 13:40:18 -0500

    Añadir las dependencias de Bootstrap y Sass y  actualizar la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,23 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          'import',
+          'color-functions',
+          'global-builtin',
+          'legacy-js-api',
+          'if-function',
+        ],
+      },
     },
   },
 })
